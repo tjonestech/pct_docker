d86896ebfa3857b989e8c51f41373efc297d0b50
tjonestech <tjones.tech@gmail.com>
2018-12-16 21:14:58 -0500

    Update pct_desktop.sh

--- setup/pct_desktop.sh
+++ setup/pct_desktop.sh
@@ -1,1 +1,3 @@
 sudo wget -qO /opt/popcorn-time/popcorntime.png goo.gl/wz8ifr
+sudo ln -sf /opt/popcorn-time/Popcorn-Time /usr/bin/Popcorn-Time
+sudo cp ./popcorntime.desktop /usr/share/applications/popcorntime.desktop
